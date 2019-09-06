5c739760c4e42893e3406ee4b4acd87bfa364fd8
MiguelAHI <47163995+MiguelAHI@users.noreply.github.com>
2019-09-05 22:52:15 -0500

    Update Untitled3.py

--- Untitled3.py
+++ Untitled3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[56]:
+# In[26]:
 
 
 import nltk
@@ -26,7 +26,7 @@ for para in paragrafos_Eng1:
     
 articulo_Eng1  = articulo_Eng1.lower()
 
-articulo_Eng1 = re.sub(r'[^a-zA-ZñÑáéíóúÁÉÍÓÚ\s]', '', articulo_Eng1)
+articulo_Eng1 = re.sub(r'[^a-zA-ZñÑáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙäëïöüÄËÏÖÜçÇßâêîôûÂÊÎÔÛ\s]', '', articulo_Eng1)
 
 ##Articulo Espaniol
 texto_html_Esp1 = urllib.request.urlopen('https://es.wikipedia.org/wiki/Rosal%C3%ADa_Vila')
@@ -41,7 +41,7 @@ for para in paragrafos_Esp1:
     
 articulo_Esp1  = articulo_Esp1.lower()
 
-articulo_Esp1= re.sub(r'[^a-zA-ZñÑáéíóúÁÉÍÓÚ\s]', '', articulo_Esp1)
+articulo_Esp1= re.sub(r'[^a-zA-ZñÑáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙäëïöüÄËÏÖÜçÇßâêîôûÂÊÎÔÛ\s]', '', articulo_Esp1)
 
 
 ##Articulo Portuges
@@ -57,36 +57,62 @@ for para in paragrafos_Port1:
     
 articulo_Port1  = articulo_Port1.lower()
 
-articulo_Esp1= re.sub(r'[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]', '', articulo_Esp1)
+articulo_Esp1= re.sub(r'[^a-zA-ZñÑáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙäëïöüÄËÏÖÜçÇßâêîôûÂÊÎÔÛ\s]', '', articulo_Esp1)
 
 ##Terminamos de leer los archivos de entrenamiento
 nGramasTotales = 0
+probIdioma = 1/3
 ngramasEng = {}
 ngramasEsp = {}
 ngramasPort = {}
+totalEsp = 0
+totalEng = 0
+totalPort = 0
 chars = 4
+##Separamos los ngramas del archivo en Ingles y sacamos sus probabilidades 
+##por ngrama en ingles
 
 for i in range(len(articulo_Eng1)-chars):
     seq = articulo_Eng1[i:i+chars]
     if seq not in ngramasEng.keys():
         ngramasEng[seq] = [1]
         nGramasTotales += 1
+        totalEng += 1
     ngramasEng[seq][0] += 1
+    
+for key, val in ngramasEng.items():
+    ngramasEng[key].append(val[0]/totalEng)
+
+##Para los Ngramas en Espanol
     
 for i in range(len(articulo_Esp1)-chars):
     seq = articulo_Esp1[i:i+chars]
     if seq not in ngramasEsp.keys():
         ngramasEsp[seq] = [1]
         nGramasTotales += 1
+        totalEsp += 1
     ngramasEsp[seq][0] += 1
 
+for key, val in ngramasEsp.items():
+    ngramasEsp[key].append(val[0]/totalEsp)
+
+##Para los Ngramas en Portuges
+    
 for i in range(len(articulo_Port1)-chars):
     seq = articulo_Port1[i:i+chars]
     if seq not in ngramasPort.keys():
         ngramasPort[seq] = [1]
         nGramasTotales += 1
+        totalPort += 1
     ngramasPort[seq][0] += 1
 
+for key, val in ngramasPort.items():
+    ngramasPort[key].append(val[0]/totalPort)
+
+    
+##Ngramas para las probabilidades de las palabras 
+
+    
 dict(list(ngramasEng.items())[0:50])
 
 
